591c3e584e021bb539b2da4a44313a0cb6d56f2b
Iris Nevins  & Quynh Nguyen <pair+cosmosiris.miss-quynh@devbootcamp.com>
2017-10-30 16:02:20 -0700

    complete user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  # users#INDEX
+  def index
+    @users = User.all
+  end
+
+  # users#SHOW
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # users#NEW
+  def new
+    @user = User.new
+  end
+
+  # users#CREATE
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to users_path
+    else
+      @errors = @user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :votes, foreign_key: :judge_id
 
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
